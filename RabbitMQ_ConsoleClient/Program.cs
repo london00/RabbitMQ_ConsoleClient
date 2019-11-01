5c5ecf269e74cb1ef1c4065d79e08c2cf5b5190e
Geiser Aragón Cejas <isc.geiser.aragon.cejas@outlook.com>
2019-11-01 10:29:56 -0600

    Add project files.

---
+++ RabbitMQ_ConsoleClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RabbitMQ_ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
